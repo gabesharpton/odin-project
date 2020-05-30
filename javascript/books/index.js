109b67a1dd7350da10f3f7ae33609fac09c7abf7
gabesharpton <gabriel.sharpton@gmail.com>
2020-05-29 20:37:02 -0500

    added skeleton for library project

---
+++ javascript/books/index.js
@@ -0,0 +1,16 @@
+let myLibrary = ["Harry Potter", "Bruce"];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+myLibrary.forEach(function(x) {
+    document.getElementById("books") = `<div>${x}</div>`}
+    )
+render(myLibrary)
+   
+console.log(books)
